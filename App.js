6473bf4cdbae3dc85ec3e4c2e457f87749eed821
Judy <850832627@qq.com>
2018-08-30 18:46:02 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './App.css'
+import { getUser } from './github'
+
+const renderLine = (user, key) => <li key={key}><b>{key}</b>: {user[key]}</li>
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { user: {} }
+  }
+
+  componentDidMount () {
+    getUser('vnglst').then(data => {
+      this.setState({ user: data.entity })
+    })
+  }
+
+  render () {
+    const { user } = this.state
+    return (
+      <div className='App'>
+        <ul style={{ listStyle: 'none' }}>
+          {
+            // Loop over the object keys and render each key
+            Object.keys(user).map(key => renderLine(user, key))
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
